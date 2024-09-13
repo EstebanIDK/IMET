be169ec254776dcdd6fdcda861d7a7a8855f33f2
EstebanIDK <estebanlm10ucl@gmail.com>
2024-09-13 01:37:50 -0300

    creando formulario producto

--- entidad/urls.py
+++ entidad/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.home, name="home"),
     path('productos/', views.productos, name="productos"),
     path('nuevo_producto/', views.nuevo_producto, name="nuevo_producto")
 ]
